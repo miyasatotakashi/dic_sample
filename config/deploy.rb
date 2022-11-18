aed07d58be19353118fee268eaed9a4621a0f7a3
takashi miyasato <on.the.line.mt215@gmail.com>
2022-11-18 15:41:03 +0900

    add platform

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,17 @@
 lock '3.16.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'dic_sample'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/miyasatotakashi/dic_sample.git'
+set :repo_url, 'https://github.com/miyasatotakashi/dic_sample'
 
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/dic_sample'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
